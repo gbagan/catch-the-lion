b2349b03f2cd42498ffbbecae47468a8d75c87ad
Guillaume Bagan <gbagan@gmail.com>
2025-10-05 22:39:26 +0200

    adapte les icones aux bonnes dimensions

--- src/components/Credits.svelte
+++ src/components/Credits.svelte
@@ -7,12 +7,10 @@
 
 <Dialog title="Crédits" onOk={closeDialog}>
   <ul>
-    <li class="list-disc"><Emph>Catch the lion</Emph> est un jeu de plateau créé
+    <li><Emph>Catch the lion</Emph> est un jeu de plateau créé
       par Madoka Kitao et Maiko Fujita.</li>
-    <li class="list-disc">L'éditeur du jeu est Nekomado.</li>
-    <li class="list-disc">L'application a été écrite par Guillaume Bagan.</li>
-    <li class="list-disc">Le code source est sous licence MIT.</li>
-    <li class="list-disc">Les différentes images ont été générées par Dall-E.</li>
-    <li class="list-disc">Les différents sons proviennent de la plate-forme Lichess.</li>
+    <li>L'éditeur du jeu est Nekomado.</li>
+    <li>Les différentes images ont été générées par Dall-E.</li>
+    <li>Les différents sons proviennent de la plate-forme Lichess.</li>
   </ul>  
 </Dialog>
--- src/components/Rules.svelte
+++ src/components/Rules.svelte
@@ -7,27 +7,27 @@
 
 <Dialog title="Règles de Catch The Lion" onOk={closeDialog}>
   <ul>
-    <li class="list-disc"><Emph>Catch the lion</Emph> est un jeu à deux joueurs sur un plateau 4x3.
+    <li><Emph>Catch the lion</Emph> est un jeu à deux joueurs sur un plateau 4x3.
       Le but est de capturer le <Emph>lion</Emph> adverse.
     </li>
-    <li class="list-disc">Chaque joueur possède 4 pièces: un <Emph>lion</Emph>, une <Emph>girafe</Emph>,
+    <li>Chaque joueur possède 4 pièces: un <Emph>lion</Emph>, une <Emph>girafe</Emph>,
         un <Emph>éléphant</Emph> et un <Emph>poussin</Emph>.
     </li>
-    <li class="list-disc">L'<Emph>orientation</Emph> d'une pièce indique à quel joueur elle appartient.</li>
-    <li class="list-disc">Chaque pièce peut se déplacer d'une case dans une des directions indiquées sur la pièce<br/>
+    <li>L'<Emph>orientation</Emph> d'une pièce indique à quel joueur elle appartient.</li>
+    <li>Chaque pièce peut se déplacer d'une case dans une des directions indiquées sur la pièce<br/>
         si la case d'arrivée est vide ou si il y a une pièce adverse dessus.
     </li>
-    <li class="list-disc">Une pièce peut <Emph>capturer</Emph> une pièce adverse en se déplaçant dessus.<br/>
+    <li>Une pièce peut <Emph>capturer</Emph> une pièce adverse en se déplaçant dessus.<br/>
         La pièce capturée va dans sa <Emph>réserve</Emph> et il peut la placer sur
         n'importe quelle case vide à partir du tour suivant.
     </li>
-    <li class="list-disc">Si un <Emph>poussin</Emph> atteind la dernière rangée, il est promu en <Emph>poule</Emph>.<br/>
+    <li>Si un <Emph>poussin</Emph> atteind la dernière rangée, il est promu en <Emph>poule</Emph>.<br/>
         Si la poule se fait capturer, elle redevient un poussin.
     </li>
-    <li class="list-disc">Un <Emph>poussin</Emph> placé depuis la réserve sur la dernière
+    <li>Un <Emph>poussin</Emph> placé depuis la réserve sur la dernière
         rangée ne devient pas une poule.
     </li>
-    <li class="list-disc">Tu peux également gagner la partie en emmenant ton <Emph>lion</Emph> sur la dernière rangée.<br/>
+    <li>Tu peux également gagner la partie en emmenant ton <Emph>lion</Emph> sur la dernière rangée.<br/>
           Le lion ne doit pas pouvoir être immédiatement capturé par l&apos;adversaire.
     </li>
   </ul>
